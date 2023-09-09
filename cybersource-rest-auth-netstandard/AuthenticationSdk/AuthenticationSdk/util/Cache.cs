9c5074651a9061e681ba17791ec4975218b62d28
monu-kumar-visa <138751796+monu-kumar-visa@users.noreply.github.com>
2023-09-09 14:06:54 +0530

    added caching

--- cybersource-rest-auth-netstandard/AuthenticationSdk/AuthenticationSdk/util/Cache.cs
+++ cybersource-rest-auth-netstandard/AuthenticationSdk/AuthenticationSdk/util/Cache.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AuthenticationSdk.core;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.OpenSsl;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Caching;
@@ -15,6 +20,10 @@ namespace AuthenticationSdk.util
         /// </summary>
         private static readonly object mutex = new object();
 
+        private static readonly object mutexForPrivateKeyFromPEM = new object();
+
+        private static readonly string regexForFileNameFromDirectory = "(^([a-z]|[A-Z]):(?=\\\\(?![\\0-\\37<>:\"/\\\\|?*])|\\/(?![\\0-\\37<>:\"/\\\\|?*])|$)|^\\\\(?=[\\\\\\/][^\\0-\\37<>:\"/\\\\|?*]+)|^(?=(\\\\|\\/)$)|^\\.(?=(\\\\|\\/)$)|^\\.\\.(?=(\\\\|\\/)$)|^(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+)|^\\.(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+)|^\\.\\.(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+))((\\\\|\\/)([^\\0-\\37<>:\"/\\\\|?*]+|(\\\\|\\/)$))*()$";
+
         public static X509Certificate2 FetchCachedCertificate(string p12FilePath, string keyPassword)
         {
             try
@@ -23,11 +32,7 @@ namespace AuthenticationSdk.util
                 {
                     ObjectCache cache = MemoryCache.Default;
 
-                    // (^([a-z]|[A-Z]):(?=\\(?![\0-\37<>:"/\\|?*])|\/(?![\0-\37<>:"/\\|?*])|$)|^\\(?=[\\\/][^\0-\37<>:"/\\|?*]+)|^(?=(\\|\/)$)|^\.(?=(\\|\/)$)|^\.\.(?=(\\|\/)$)|^(?=(\\|\/)[^\0-\37<>:"/\\|?*]+)|^\.(?=(\\|\/)[^\0-\37<>:"/\\|?*]+)|^\.\.(?=(\\|\/)[^\0-\37<>:"/\\|?*]+))((\\|\/)[^\0-\37<>:"/\\|?*]+|(\\|\/)$)*()$
-
-                    var pattern = "(^([a-z]|[A-Z]):(?=\\\\(?![\\0-\\37<>:\"/\\\\|?*])|\\/(?![\\0-\\37<>:\"/\\\\|?*])|$)|^\\\\(?=[\\\\\\/][^\\0-\\37<>:\"/\\\\|?*]+)|^(?=(\\\\|\\/)$)|^\\.(?=(\\\\|\\/)$)|^\\.\\.(?=(\\\\|\\/)$)|^(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+)|^\\.(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+)|^\\.\\.(?=(\\\\|\\/)[^\\0-\\37<>:\"/\\\\|?*]+))((\\\\|\\/)([^\\0-\\37<>:\"/\\\\|?*]+|(\\\\|\\/)$))*()$";
-
-                    var matches = Regex.Match(p12FilePath, pattern);
+                    var matches = Regex.Match(p12FilePath, regexForFileNameFromDirectory);
                     var certFile = matches.Groups[11].ToString();
 
                     if (!cache.Contains(certFile))
@@ -60,6 +65,35 @@ namespace AuthenticationSdk.util
                 }
 
                 throw e;
+            }
+        }
+
+        public static RSAParameters FetchCachedRSAParameters(MerchantConfig merchantConfig)
+        {
+            lock (mutexForPrivateKeyFromPEM)
+            {
+                var pemFilePath = merchantConfig.PemFileDirectory;
+                ObjectCache cache = MemoryCache.Default;
+
+                var matches = Regex.Match(merchantConfig.PemFileDirectory, regexForFileNameFromDirectory);
+                var certFile = matches.Groups[11].ToString();
+
+                if (!cache.Contains(certFile))
+                {
+                    var policy = new CacheItemPolicy();
+                    var filePaths = new List<string>();
+                    var privateKey = File.ReadAllText(merchantConfig.PemFileDirectory);
+                    var cachedFilePath = Path.GetFullPath(pemFilePath);
+                    filePaths.Add(cachedFilePath);
+                    policy.ChangeMonitors.Add(new HostFileChangeMonitor(filePaths));
+
+                    PemReader pemReader = new PemReader(new StringReader(privateKey));
+                    AsymmetricCipherKeyPair keyPair = (AsymmetricCipherKeyPair)pemReader.ReadObject();
+                    RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)keyPair.Private);
+
+                    cache.Set(certFile, rsaParams, policy);
+                }
+                return (RSAParameters)cache[certFile];
             }
         }
     }
